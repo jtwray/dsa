1a55486b3d7948e8aa44738c05455e4f2fb7fe43
tucker <tuckerwray84@gmail.com>
2023-02-12 17:51:30 -0500

    describes plan in a comment  within waterplants.js

--- dfs/waterplants/waterplants.js
+++ dfs/waterplants/waterplants.js
@@ -16,13 +16,30 @@ function isValid(
 ) {
 
 }
-function waterplants(pot) {
+function waterPlants(pot) {
     let potNodesLength = 0;
     pot.map(hall => hall.map(room => potNodesLength++));
 
-    dfsMatrix(pot[0][0],pot,)
+    dfsMatrix(pot[0][0], pot,)
 
 }
+
+
+
+// dfs with traverse ever node going deep before wide using a stack
+// a more performant version could save time and stack space by returning early if the current node is within the bottom row
+// a basic approach would be to dfs all nodes and return  a list for each open path of 0's
+// or return back the entire traversal path and then loop through the traversal path once looking for a node with the bottom row
+
+//steps
+
+// implement dfs 
+// traverse the graph 
+// return traversal path
+// check it for a node with row = graph.length-1
+// return true if found else return false
+
+
 
 
 
