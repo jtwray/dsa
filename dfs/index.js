b6e4e724dae80a0d89dcf70bfc541a81fdb7a4a3
tucker <tuckerwray84@gmail.com>
2023-02-17 10:32:24 -0500

    feat: adds pseudocode DFS

---
+++ dfs/index.js
@@ -0,0 +1,40 @@
+// // pass node to dfs()
+// // push node to stack in upper scope
+// // while loop 
+// // - if stack !empty
+// //  -- node=stack.pop()
+// // 
+
+
+// // pass curr to dfs()
+// // push curr onto stack
+// // loop while stack is not empty
+// // set curr to stack.length-1
+// // pop last off stack
+// // check for curr in visited
+// // if curr not in visited
+// // get curr
+
+// // visite the adjacent unvisited vertex mark as visited print it out
+// // if no asjecent vertex pop one from stack
+
+
+
+
+// visit node
+
+// add to stack
+
+// while stack is not empty
+// set curr to stack.length-1
+// stack.pop()
+
+
+// if curr is not visited
+
+// add curr to visited
+
+// currNeighbors=neighbors[curr]
+
+// loop currNeighbors
+// add all not in visited tostack
